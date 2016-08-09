93306c8bb0a6b7dc9670f5d3a3a208c3dbea0853
Adam Pahlevi <adam.pahlevi@gmail.com>
2016-08-09 21:34:21 +0700

    implement hello world
    key takeaways:
    
    - define routes to / to match the application#index
    - move application.js loading in the layout after the site's tag is already printed out
    - create dummy HelloWorld Leaderboard component

---
+++ client/components/leaderboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Leaderboard extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+            </div>
+        );
+    }
+}
--- client/index.jsx
+++ client/index.jsx
@@ -1,1 +1,6 @@
-var helloWorld = 'helloWorld';
+import React from 'react';
+import Leaderboard from './components/leaderboard.jsx';
+import { render } from 'react-dom';
+
+let renderPlace = document.getElementById('app');
+render(<Leaderboard/>, renderPlace);
