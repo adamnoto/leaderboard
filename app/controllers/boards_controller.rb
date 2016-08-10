9cf8bac211ea5cd3631bbc4d047105cbbc354fe1
Adam Pahlevi <adam.pahlevi@gmail.com>
2016-08-10 22:17:17 +0700

    backend code to add vote

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -5,4 +5,11 @@ class BoardsController < ActionController::API
     boards.map! { |attr| attr.slice('id', 'name', 'total_vote') }
     render json: boards
   end
+
+  def vote
+    id = params[:selectedLeaderId]
+    board = Board.find(id)
+    board.total_vote += 5
+    board.save!
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'application#index'
 
   resources :boards, only: [:index]
+  post 'boards/vote', to: 'boards#vote'
 end
