8c3eee2f9f792fa2bd3be77c9695337ea277ccb2
Adam Pahlevi <adam.pahlevi@gmail.com>
2016-08-10 09:19:24 +0700

    npm install axios as dep

---
+++ client/components/board-list.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import _ from 'lodash';
+import BoardActions from '../actions/board-actions.react';
+import BoardStore from '../stores/board-store.react';
+
+export default class BoardList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            collection: []
+        };
+
+        this.onChange = this.onChange.bind(this);
+
+        BoardActions.index();
+    }
+
+    componentDidMount() {
+        BoardStore.addChangeListener(this.onChange);
+    }
+
+    componentWillUnmount() {
+        BoardStore.removeChangeListener(this.onChange);
+    }
+
+    onChange() {
+        this.setState({collection: BoardStore.getCollection()});
+    }
+
+    render() {
+        return (
+            <div className='leaderboard'>
+                <div className='playerList'>
+                    {this.state.collection.map((data) => (
+                        <Leader id={data.id}
+                            name={data.name}
+                        ></Leader>
+                    ))}
+                </div>
+            </div>
+        )
+    }
+}
